f6d9da733c33e24426727d96bf8e1d8acaf7ed79
liyongqi <2297940456@qq.com>
2024-07-06 18:47:28 +0800

    classpath cmd

--- jvmgo/ch01/main.go
+++ jvmgo/ch01/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	cmd := parseCmd()
 	if cmd.versionFlag {
-		fmt.Println("verson 0.0.1")
+		fmt.Println("version 0.0.1")
 	} else if cmd.helpFlag || cmd.class == "" {
 		printUsage()
 	} else {
---
+++ jvmgo/ch02/classpath/classpath.go
@@ -0,0 +1,81 @@
+package classpath
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type Classpath struct {
+	bootClasspath Entry
+	extClasspath  Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+	// 启动类路径 和 扩展类路径
+	cp.parseBootAndExtClasspath(jreOption)
+	// 用户类路径
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+
+/**
+ * 优先使用用户输入的-Xjre 作为jre目录
+ * 没有输入则选择当下目录找寻jre目录
+ */
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	// jre/lib/*
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	// jre/lib/ext/*
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+	if exists(".jre") {
+		return ".jre"
+	}
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+	panic("Can not find jre folder!")
+}
+
+// 判断目录是否存在
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// TODO debug看下这个newEntry怎么走的
+func (self *Classpath) parseUserClasspath(cpOption string) {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
+
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+	className = className + ".class"
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	return self.userClasspath.readClass(className)
+}
+
+func (self *Classpath) String() string {
+	return self.userClasspath.String()
+}
